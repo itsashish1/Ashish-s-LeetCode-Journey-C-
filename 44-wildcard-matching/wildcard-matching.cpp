99feac9087ca7fec4ca62777bb902ae7f2ad0b8d
Ashish Yadav <gtc.ashish1@gmail.com>
2026-03-11 12:17:14 +0530

    Time: 2 ms (88.57%) | Memory: 9.8 MB (86.90%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int i = 0, j = 0;
+        int starIndex = -1, match = 0;
+
+        while(i < s.size()) {
+
+            if(j < p.size() && (p[j] == s[i] || p[j] == '?')) {
+                i++;
+                j++;
+            }
+
+            else if(j < p.size() && p[j] == '*') {
+                starIndex = j;
+                match = i;
+                j++;
+            }
+
+            else if(starIndex != -1) {
+                j = starIndex + 1;
+                match++;
+                i = match;
+            }
+
+            else {
+                return false;
+            }
+        }
+
+        while(j < p.size() && p[j] == '*')
+            j++;
+
+        return j == p.size();
+    }
+};
