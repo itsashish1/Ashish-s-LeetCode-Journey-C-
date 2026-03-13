fad2f325a39f6af68bb828ccfd15b838a2754ab3
Ashish Yadav <gtc.ashish1@gmail.com>
2026-03-13 19:10:59 +0545

    Time: 0 ms (100.00%) | Memory: 10.5 MB (87.65%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void backtrack(vector<int>& nums, vector<vector<int>>& result, vector<int>& temp, vector<bool>& used) {
+        if (temp.size() == nums.size()) {
+            result.push_back(temp);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (used[i]) continue;
+
+            used[i] = true;
+            temp.push_back(nums[i]);
+
+            backtrack(nums, result, temp, used);
+
+            temp.pop_back();
+            used[i] = false;
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> temp;
+        vector<bool> used(nums.size(), false);
+
+        backtrack(nums, result, temp, used);
+        return result;
+    }
+};
