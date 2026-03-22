415c0c4ee52458f39fbc7a93990bfe1fd90962b3
Ashish Yadav <gtc.ashish1@gmail.com>
2026-03-22 21:27:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (44.28%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n; // to handle INT_MIN
+        
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+
+        double result = 1.0;
+
+        while (N > 0) {
+            if (N % 2 == 1) {
+                result *= x;
+            }
+            x *= x;
+            N /= 2;
+        }
+
+        
+
+        return result;
+    }
+};
