7fb9f6eef87ef67d38bf8f6fc7307e67058bfd35
Ashish Yadav <gtc.ashish1@gmail.com>
2026-03-27 01:12:45 +0530

    Time: 10 ms (15.07%) | Memory: 25 MB (8.57%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        // Step 1: Sort intervals
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> result;
+
+        for (auto interval : intervals) {
+            // Step 2: If no overlap
+            if (result.empty() || result.back()[1] < interval[0]) {
+                result.push_back(interval);
+            } 
+            else {
+                // Step 3: Merge intervals
+                result.back()[1] = max(result.back()[1], interval[1]);
+            }
+        }
+
+        return result;
+    }
+};
