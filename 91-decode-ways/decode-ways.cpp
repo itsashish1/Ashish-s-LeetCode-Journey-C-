9458986ebab144dc5225bb223593cf6282e9aded
Ashish Yadav <gtc.ashish1@gmail.com>
2026-05-16 12:03:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (79.03%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.length();
+
+        // If string starts with 0, no valid decoding
+        if (s[0] == '0')
+            return 0;
+
+        vector<int> dp(n + 1, 0);
+
+        // Base cases
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+
+            // Single digit decode (1-9)
+            int oneDigit = s[i - 1] - '0';
+            if (oneDigit >= 1 && oneDigit <= 9) {
+                dp[i] += dp[i - 1];
+            }
+
+            // Two digit decode (10-26)
+            int twoDigit = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+};
