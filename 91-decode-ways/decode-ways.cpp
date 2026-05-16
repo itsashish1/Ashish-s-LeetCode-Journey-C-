6e3ca4955511dde9388ef355a35d935cfb24bc34
Ashish Yadav <gtc.ashish1@gmail.com>
2026-05-16 12:03:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (50.29%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -25,12 +25,14 @@ public:
                 dp[i] += dp[i - 1];
             }
 
+            
+
             // Two digit decode (10-26)
             int twoDigit = (s[i - 2] - '0') * 10 + (s[i - 1] - '0');
             if (twoDigit >= 10 && twoDigit <= 26) {
                 dp[i] += dp[i - 2];
             }
-            
+
         }
 
         return dp[n];
