a805a594fdc3f55b317292d1b8b80a211a2c9c50
Ashish Yadav <gtc.ashish1@gmail.com>
2026-05-19 23:15:14 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (52.95%) - LeetSync

---
+++ 89-gray-code/gray-code.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int> result;
+        
+        // Gray Code Formula: i ^ (i >> 1)
+        for (int i = 0; i < (1 << n); i++) {
+            result.push_back(i ^ (i >> 1));
+        }
+
+        
+        
+        return result;
+    }
+};
